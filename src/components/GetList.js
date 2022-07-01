796e18a07cd98fb2006a7fed54b9d764e4fd3784
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-01 14:49:13 +0530

    Todo_complete

--- src/components/GetList.js
+++ src/components/GetList.js
@@ -25,24 +25,36 @@ const GetList = () => {
     }
 
     const showStatus=(temp)=>{
-        return((temp === 'false')?<label>Not Done Yet</label>:<label>Already done </label>)
+        return((temp === 'false')?<label className='text-red-500'> Not Done Yet</label>:<label className='text-green-500'> Already done </label>)
     }
 
     return ( 
-        <div>
+        <div className='flex flex-col items-center justify-center' >
             <br/>
             <br/>
             <button className='font-mono rounded-md w-[fit-content] px-2 text-[20px] bg-slate-100 hover:shadow-lg' onClick={getList} >Get List of work</button>
+            <br/>
+            <div className='w-[50%] h-[fit-content] shadow-xl rounded-xl bg-white p-[30px]'>
+                <label className='text-[20px] font-mono text-white bg-black w-[fit-content] py-2 px-4 rounded-xl'>Your Work List</label>
+                <br/>
+                <br/>
+            <div className='flex flex-col-reverse w-[100%] h-[fit-content]' >
             {
-                Object.keys(lists).map((list,index)=>(
-                    <div>
-                    <p>{lists[index].work}</p>
-                    <p>Status:{(lists[index].done).toString()}</p>
-                    <button onClick={()=>change(index)}  >Change</button>
-                    {showStatus((lists[index].done).toString())}
+                 Object.keys(lists).map((list,index)=>(
+                    <div className=''>
+                        <div className='flex flex-row w-[100%] justify-around border-2 rounded-md p-2 '>
+                        <p className='font-mono font-semi-bold text-[20px]'>{lists[index].work}</p>
+                        <p className='text-[20px]'>Status : {showStatus((lists[index].done).toString())}</p>
+                        </div>
+                    <br/>
+                    <button className='bg-slate-100 w-[fit-content] p-1 rounded-md hover:shadow' onClick={()=>change(index)}>Change Status</button>
+                    <br/>
+                    <br/>
                     </div>
                 ))
             }
+            </div>
+            </div>
         </div>
      );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -11,7 +11,7 @@ const List = () => {
     return ( 
         <div>
             <br/>
-            <label className='text-[40px]'>Welcome to Your Daily Work-Tracker</label>
+            <label className='text-[50px]'>Welcome to Your Daily Work-Tracker</label>
             <br/>
             <br/>
             <br/>
@@ -19,7 +19,7 @@ const List = () => {
             <input className="w-[400px] h-[50px] rounded-md" onChange={e => setTodo(e.target.value)}/>
             <br/>
             <br/>
-            <button className='w-[100px] text-purple-900 text-[20px] bg-slate-100 rounded-md hover:shadow-xl' onClick={addTodo} >Add Work</button>
+            <button className='w-[100px] text-purple-900 text-[20px] bg-white rounded-md hover:shadow-xl' onClick={addTodo} >Add Work</button>
         </div>
      );
 }
